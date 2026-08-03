bd3659b1ded8f2f8c89db74352870218062b5985
Elliot <elliot.canter@cooper.edu>
2026-08-03 12:19:22 -0400

    version 4

--- scripts/sample_ddpm_cifar10_pruned.sh
+++ scripts/sample_ddpm_cifar10_pruned.sh
@@ -1,6 +1,6 @@
 python ddpm_sample.py \
 --output_dir run/sample/ddpm_cifar10_pruned \
---batch_size 128 \
+--batch_size ${2:-128} \
 --total_samples ${1:-50000} \
 --pruned_model_ckpt run/finetuned/ddpm_cifar10_pruned_post_training/pruned/unet_ema_pruned.pth \
 --model_path run/finetuned/ddpm_cifar10_pruned_post_training \
